bd1708338251c3c6ceb613e4aaa82aaefc3568e0
Chris Ainslie <github@ainslie.co.za>
2018-03-08 08:38:28 +0200

    updated port number for Azure deployment

--- HomeGateway/server.js
+++ HomeGateway/server.js
@@ -2,10 +2,10 @@
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 80;
 var app = express();
 
-app.set('port', process.env.PORT || 1337);
+app.set('port', process.env.PORT || 80);
 
 app.get('/', routes.index);
 app.get('/createnewdevice/:deviceID/:devicename/:outputPin/:outputMode/:outputDuration', routes.newDevice);
